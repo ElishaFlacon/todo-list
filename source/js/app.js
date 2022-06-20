934c8c805cca79b1c9ca8b4f8c25bd7e08d15798
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2022-06-20 23:32:54 +0500

    start js coding

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,15 @@
-// ??
+// Переменныe
+const todoInput = document.querySelector('.todo-form__input');
+const todoButton = document.querySelector('.todo-form__button');
+const todoList = document.querySelector('.todo-list');
+
+// Ивенты
+todoButton.addEventListener('click', addTodo);
+
+
+// Функции
+function addTodo(event) {
+    // Очищаем ивенты
+    event.preventDefault();
+    // Создаем блок с листом
+}
