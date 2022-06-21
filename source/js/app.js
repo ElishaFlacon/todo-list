5a417e790fc6143b3a7393ef2fbef1900df6d11c
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2022-06-21 14:29:04 +0500

    addTodo, removeTodo functions

--- source/js/app.js
+++ source/js/app.js
@@ -5,11 +5,40 @@ const todoList = document.querySelector('.todo-list');
 
 // Ивенты
 todoButton.addEventListener('click', addTodo);
-
+todoList.addEventListener('click', deleteCheck);
 
 // Функции
 function addTodo(event) {
     // Очищаем ивенты
     event.preventDefault();
-    // Создаем блок с листом
+    // Создаем div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //  Создаем li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo__item');
+    todoDiv.appendChild(newTodo);
+    //  Создаем кнопку завершения задачи
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa-regular fa-square-check"></i>';
+    completedButton.classList.add('todo__complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Создаем кнопку удаления задачи
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa-regular fa-square-minus"></i>';
+    deleteButton.classList.add('todo__delete-btn');
+    todoDiv.appendChild(deleteButton);
+    // Добовляем элементы на страницу
+    todoList.appendChild(todoDiv);
+    //  Очищаем инпут
+    todoInput.value = '';
+}
+
+function deleteCheck(event) {
+    const item = event.target;
+    if (item.classList[0] === 'todo__delete-btn') {
+        const todo = item.parentElement;
+        todo.remove();
+    };
 }
