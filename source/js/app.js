ee00097865de669fdd8c4942545af2c7143bb4c7
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2022-08-16 17:31:55 +0500

    add working on data function

--- source/js/app.js
+++ source/js/app.js
@@ -4,10 +4,13 @@ const todoButton = document.querySelector('.todo-form__button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 
+
 // Ивенты
+document.addEventListener('DOMContentLoaded', getData);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('click', filter)
+
 
 // Функции
 function addTodo(event) {
@@ -21,6 +24,8 @@ function addTodo(event) {
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo__item');
     todoDiv.appendChild(newTodo);
+    // Закидываем тудушки в LocalХранилище
+    saveData(todoInput.value);
     //  Создаем кнопку завершения задачи
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fa-regular fa-square-check"></i>';
@@ -37,12 +42,14 @@ function addTodo(event) {
     todoInput.value = '';
 }
 
+
 function deleteCheck(event) {
     const item = event.target;
     // Удаление
     if (item.classList[0] === 'todo__delete-btn') {
         const todo = item.parentElement;
         todo.classList.add('fall');
+        clearData(todo)
         todo.addEventListener('transitionend', function () {
             todo.remove();
         });
@@ -53,6 +60,7 @@ function deleteCheck(event) {
         todo.classList.toggle('completed');
     };
 }
+
 
 function filter(event) {
     const todos = todoList.childNodes;
@@ -77,4 +85,58 @@ function filter(event) {
                 break;
         }
     });
+}
+
+
+function checkData() {
+    // Проверяем наличие данных
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    return todos;
+}
+
+
+function saveData(data) {
+    let todos = checkData()
+    todos.push(data);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+
+function getData() {
+    let todos = checkData()
+    todos.forEach(function (todo) {
+        // Создаем div
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        //  Создаем li
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo;
+        newTodo.classList.add('todo__item');
+        todoDiv.appendChild(newTodo);
+        //  Создаем кнопку завершения задачи
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fa-regular fa-square-check"></i>';
+        completedButton.classList.add('todo__complete-btn');
+        todoDiv.appendChild(completedButton);
+        // Создаем кнопку удаления задачи
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fa-regular fa-square-minus"></i>';
+        deleteButton.classList.add('todo__delete-btn');
+        todoDiv.appendChild(deleteButton);
+        // Добовляем элементы на страницу
+        todoList.appendChild(todoDiv);
+    });
+}
+
+
+function clearData(data) {
+    let todos = checkData()
+    const todoIndex = data.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
