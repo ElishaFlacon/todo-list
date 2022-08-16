96d56c311bea5aa772b644383fc3c3c932142ce0
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2022-08-16 18:24:18 +0500

    add adaptability

--- source/js/app.js
+++ source/js/app.js
@@ -13,6 +13,14 @@ filterOption.addEventListener('click', filter)
 
 
 // Функции
+
+function limitText(limitField, limitNum) {
+    if (limitField.value.length > limitNum) {
+        limitField.value = limitField.value.substring(0, limitNum);
+    }
+}
+
+
 function addTodo(event) {
     // Очищаем ивенты
     event.preventDefault();
