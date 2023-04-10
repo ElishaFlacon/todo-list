eff522b1566b45f87539fe9bbbc264c6d1bdd05d
Elisha Flacon <elishaflacon@gmail.com>
2023-04-10 12:42:25 +0500

    added completed state for todos in cookies теперь задачи помеченные как выполненные будут сохранять свое состояние при перезагрузке страницы

--- source/js/app.js
+++ source/js/app.js
@@ -33,7 +33,7 @@ function addTodo(event) {
     newTodo.classList.add('todo__item');
     todoDiv.appendChild(newTodo);
     // Закидываем тудушки в LocalХранилище
-    saveData(todoInput.value);
+    saveData([todoInput.value, 0]);
     //  Создаем кнопку завершения задачи
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fa-regular fa-square-check"></i>';
@@ -57,7 +57,7 @@ function deleteCheck(event) {
     if (item.classList[0] === 'todo__delete-btn') {
         const todo = item.parentElement;
         todo.classList.add('fall');
-        clearData(todo)
+        clearData(todo.children[0].innerText)
         todo.addEventListener('transitionend', function () {
             todo.remove();
         });
@@ -66,6 +66,15 @@ function deleteCheck(event) {
     if (item.classList[0] === 'todo__complete-btn') {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
+        let todos = checkData();
+        todos.map((t) => {
+            if (t[0] === todo.innerText) {
+                t[1] == 0 ? t[1] = 1 : t[1] = 0;
+
+                clearData(t[0]);
+                saveData(t);
+            }
+        });
     };
 }
 
@@ -123,8 +132,11 @@ function getData() {
         todoDiv.classList.add('todo');
         //  Создаем li
         const newTodo = document.createElement('li');
-        newTodo.innerText = todo;
+        newTodo.innerText = todo[0];
         newTodo.classList.add('todo__item');
+        if (todo[1]) {
+            todoDiv.classList.toggle('completed');
+        }
         todoDiv.appendChild(newTodo);
         //  Создаем кнопку завершения задачи
         const completedButton = document.createElement('button');
@@ -144,7 +156,12 @@ function getData() {
 
 function clearData(data) {
     let todos = checkData()
-    const todoIndex = data.children[0].innerText;
+    let todoIndex = '';
+    todos.forEach((todo) => {
+        if (todo[0] === data) {
+            todoIndex = todo;
+        }
+    })
     todos.splice(todos.indexOf(todoIndex), 1);
     localStorage.setItem('todos', JSON.stringify(todos));
 }
